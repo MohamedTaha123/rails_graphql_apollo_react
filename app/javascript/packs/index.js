768be4e680e83a65dc8e58bc9707c6eb1ee510b5
taha <mohamedtaha.boutayeb@gmail.com>
2020-09-03 02:18:41 +0100

    fix: fix babel preset react

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,1 +1,12 @@
-console.log('👻');
+import React from 'react';
+import { render } from 'react-dom';
+import Provider from '../components/Provider';
+import Library from '../components/Library';
+
+render(
+  <Provider>
+    <Library />
+  </Provider>,
+  document.querySelector('#root')
+);
+
